fae9fd147aff70c78b7e167a56d6594adf0053b9
Tyler Saunders <tylersaunders@google.com>
2025-06-04 11:02:16 +0000

    test: Use minute intervals for fake data timestamps
    - To help deflake tests, lower the intervals of fake data beign
      generated for the grid so the grid structure remains stable day over
      day, but elements are still ordered in a predictable way.
    
    Bug: b/422162308
    Test: atest PhotopickerTests
    Flag: EXEMPT test_only
    Change-Id: Id0105eb4bf2f6e73a0c0420265e315bca1e36d9f

--- photopicker/tests/src/com/android/photopicker/data/paging/FakeInMemoryMediaPagingSource.kt
+++ photopicker/tests/src/com/android/photopicker/data/paging/FakeInMemoryMediaPagingSource.kt
@@ -83,7 +83,7 @@ private constructor(
                                     .build(),
                             dateTakenMillisLong =
                                 currentDateTime
-                                    .minus(i.toLong(), ChronoUnit.DAYS)
+                                    .minus(i.toLong(), ChronoUnit.MINUTES)
                                     .toEpochSecond(ZoneOffset.UTC) * 1000,
                             sizeInBytes = 1000L,
                             mimeType = "image/png",
