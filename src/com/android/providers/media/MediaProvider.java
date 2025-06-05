17ebbe43efe36a7c00d3941513d5caf2db91f628
darshilshah <darshilshah@google.com>
2025-06-05 17:08:12 +0000

    Move markMediaAsFavorite's logic from MediaStore to MediaProvider
    The content values retrieved from extras in the MediaProvider can have
    values preset by the calling app and can affect other fields set.
    Instead of retrieving the content values, we only retrieve a boolean
    field that says whether the is_favorite is to be set to true or false.
    All processing logic is shifted to MediaProvider.
    
    Bug: 417194323
    Test: EXEMPT Bugfix
    Flag: EXEMPT Bugfix
    Change-Id: I0227139b0a04efcb7456557fbf2d47996c017bad

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -7853,8 +7853,9 @@ public class MediaProvider extends ContentProvider {
     }
 
     private Bundle markMediaAsFavorite(Bundle extras) {
-        final ContentValues values = extras.getParcelable(MediaStore.EXTRA_CONTENT_VALUES);
+        final boolean areFavorites = extras.getBoolean(MediaColumns.IS_FAVORITE);
         final ClipData clipData = extras.getParcelable(MediaStore.EXTRA_CLIP_DATA);
+
         final List<Uri> uris = collectUris(clipData);
 
         if (!isCallingPackageManager()) {
@@ -7865,6 +7866,13 @@ public class MediaProvider extends ContentProvider {
                             + " does not have required permission to mark media as favorite");
                 }
             }
+        }
+
+        final ContentValues values = new ContentValues();
+        if (areFavorites) {
+            values.put(MediaColumns.IS_FAVORITE, 1);
+        } else {
+            values.put(MediaColumns.IS_FAVORITE, 0);
         }
 
         final LocalCallingIdentity token = clearLocalCallingIdentity();
