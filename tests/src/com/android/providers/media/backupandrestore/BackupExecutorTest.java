5297bc89ee7dca4ccd8e7e8a89927db670ab8984
darshilshah <darshilshah@google.com>
2025-06-12 12:40:36 +0000

    Remove level db path does not exist checks for B&R tests
    Bug: 424418911
    Test: EXEMPT removing unnecessary assumptions in tests
    Flag: EXEMPT removing unnecessary assumptions in tests
    Change-Id: I6b0a30dfdd008570a3f8347141b417602c6d8250

--- tests/src/com/android/providers/media/backupandrestore/BackupExecutorTest.java
+++ tests/src/com/android/providers/media/backupandrestore/BackupExecutorTest.java
@@ -126,7 +126,6 @@ public final class BackupExecutorTest {
     @SdkSuppress(minSdkVersion = Build.VERSION_CODES.BAKLAVA)
     public void testBackup() throws Exception {
         assumeTrue(isBackupAndRestoreSupported(mIsolatedContext));
-        assumeFalse((new File(mLevelDbPath)).exists());
 
         try {
             // Add all files in Downloads directory
@@ -219,7 +218,6 @@ public final class BackupExecutorTest {
     public void testLevelDbRecreatedOnVersionChange() throws Exception {
         assumeTrue(isBackupAndRestoreSupported(mIsolatedContext));
         assumeTrue(Flags.enableVersioningForBackupAndRestore());
-        assumeFalse((new File(mLevelDbPath)).exists());
 
         try {
             // Add all files in Downloads directory
@@ -256,7 +254,6 @@ public final class BackupExecutorTest {
             maxSdkVersion = Build.VERSION_CODES.VANILLA_ICE_CREAM)
     public void testBackupDeletedForSdkLevelsLessThanB() {
         assumeFalse(isBackupAndRestoreSupported(mIsolatedContext));
-        assumeFalse((new File(mLevelDbPath)).exists());
 
         // create a new leveldb for backup
         LevelDBManager.getInstance(mLevelDbPath);
